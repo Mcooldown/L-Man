9539fd397d1a3a0c44c7ee10c864d61562cb45fd
Mcooldown <vincenthadinata30@gmail.com>
2021-06-11 13:11:50 +0700

    sidebar fix

--- resources/views/components/navbar-user.blade.php
+++ resources/views/components/navbar-user.blade.php
@@ -4,8 +4,8 @@
             <span class="color-teal mx-3" id="show-sidebar" style="cursor: pointer;">
                 <h2 class="text-white">☰</h2>
             </span>
-            <a class="navbar-brand" href="#">
-                <img src="" height="35px">
+            <a class="navbar-brand" href="/dashboard">
+                <img src="/storage/assets/logo.png" class="img-navbar">
             </a>
         </div>
     </nav>
